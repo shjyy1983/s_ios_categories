41ba46094d9b1a9a49fd2161b78b7ecbc1423a06
hs <hs@hs-hj.local>
2018-01-03 11:29:47 +0800

    add some

---
+++ s_ios_categories/category/Foundation/NSString/String+HJSize.swift
@@ -0,0 +1,26 @@
+//
+//  String+HJSize.swift
+//  s_ios_categories
+//
+//  Created by hs on 2018/1/3.
+//  Copyright © 2018年 SHEN HJ. All rights reserved.
+//
+
+import UIKit
+
+public extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstraintedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+    
+}
