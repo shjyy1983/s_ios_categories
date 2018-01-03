fae7f958802026e32ceffae95d1f32ca7ebeae46
hs <hs@hs-hj.local>
2018-01-03 09:44:18 +0800

    调整结构

---
+++ s_ios_categories/category/HJCategories.h
@@ -0,0 +1,14 @@
+//
+//  HJCategories.h
+//  s_ios_categories
+//
+//  Created by hs on 2018/1/3.
+//  Copyright © 2018年 SHEN HJ. All rights reserved.
+//
+
+#ifndef HJCategories_h
+#define HJCategories_h
+
+#import "HJFoundation.h"
+
+#endif /* HJCategories_h */
---
+++ s_ios_categories/category/HJFoundation.h
@@ -0,0 +1,16 @@
+//
+//  HJFoundation.h
+//  s_ios_categories
+//
+//  Created by hs on 2018/1/3.
+//  Copyright © 2018年 SHEN HJ. All rights reserved.
+//
+
+#ifndef HJFoundation_h
+#define HJFoundation_h
+
+#import "NSString+HJSize.h"
+#import "NSString+HJEncrypt.h"
+#import "NSString+HJPinyin.h"
+
+#endif /* HJFoundation_h */
