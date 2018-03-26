1799f7a3787ff436390f83b8edaaddef70b8e631
hs <>
2018-03-26 16:27:25 +0800

    dev

--- s_ios_categories/Classes/Swift/UIColor+hex.swift
+++ s_ios_categories/Classes/Swift/UIColor+hex.swift
@@ -24,4 +24,15 @@ public extension UIColor {
             blue: rgb & 0xFF
         )
     }
+    
+    func trans2Image() -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(self.cgColor)
+        context?.fill(rect)
+        let theImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return theImage ?? UIImage()
+    }
 }
